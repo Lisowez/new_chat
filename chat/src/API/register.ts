858e77f39df4c43717f2ab4cc49f0d6b09530864
Lisowez <lisowez@icloud.com>
2024-11-22 12:16:19 +0300

    refactor: refactor register

---
+++ chat/src/API/register.ts
@@ -0,0 +1,64 @@
+export interface RegisterProps {
+  username: string;
+  password: string;
+  setRegistrationMessage: (message: string) => void;
+  navigate: (path: string) => void;
+}
+
+export const register = async ({
+  username,
+  password,
+  setRegistrationMessage,
+  navigate,
+}: RegisterProps) => {
+  try {
+    // Первый запрос для создания сессии
+    const response = await fetch(
+      "https://matrix-test.maxmodal.com/_matrix/client/v3/register?kind=user",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+        }),
+      }
+    );
+    const data = await response.json();
+    // Второй запрос с использованием session
+    const secondResponse = await fetch(
+      "https://matrix-test.maxmodal.com/_matrix/client/v3/register?kind=user",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          auth: {
+            session: data.session,
+            type: "m.login.dummy",
+          },
+          refresh_token: true,
+          username: username,
+          password: password,
+        }),
+      }
+    );
+    const secondData = await secondResponse.json();
+    if (!secondResponse.ok) {
+      throw new Error(secondData.error || "Ошибка регистрации");
+    }
+    // Успешно зарегистрированы
+    setRegistrationMessage("Успешно зарегистрирован!");
+    sessionStorage.setItem("access_token", secondData.access_token);
+    sessionStorage.setItem("refreshToken", secondData.refresh_token);
+    sessionStorage.setItem("user", secondData.user_id);
+    navigate("/chat");
+    // Очистка состояния сессии
+  } catch (error) {
+    setRegistrationMessage(`Ошибка регистрации: ${error.message}`);
+    console.error("Ошибка регистрации:", error);
+  }
+};
--- chat/src/Register/Register.tsx
+++ chat/src/Register/Register.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { register } from "../API/register";
 
 const Register = () => {
   const [username, setUsername] = useState("");
@@ -7,67 +8,7 @@ const Register = () => {
   const [registrationMessage, setRegistrationMessage] = useState("");
 
   const navigate = useNavigate();
-  const [session, setSession] = useState("");
 
-  // Функция для регистрации нового пользователя
-  const register = async () => {
-    try {
-      const response = await fetch(
-        "https://matrix-test.maxmodal.com/_matrix/client/v3/register?kind=user",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: username,
-            password: password,
-          }),
-        }
-      );
-      const data = await response.json();
-      if (response) {
-        console.log(data.session);
-        setSession(data.session);
-        if (session) {
-          const response = await fetch(
-            "https://matrix-test.maxmodal.com/_matrix/client/v3/register?kind=user",
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                auth: {
-                  session: session,
-                  type: "m.login.dummy",
-                },
-                refresh_token: true,
-                username: username,
-                password: password,
-              }),
-            }
-          );
-          const data = await response.json();
-          if (response.ok) {
-            setRegistrationMessage("Успешно зарегистрирован!");
-            sessionStorage.setItem("accessToken", data.access_token);
-            sessionStorage.setItem("refreshToken", data.refresh_token);
-            navigate("/chat");
-            setSession("");
-            console.log("Пользователь успешно зарегистрирован!", data);
-          } else {
-            throw new Error(data.error || "Ошибка регистрации");
-          }
-        }
-      } else {
-        throw new Error(data.error || "Ошибка регистрации");
-      }
-    } catch (error) {
-      setRegistrationMessage(`Ошибка регистрации: ${error.message}`);
-      console.error("Ошибка регистрации:", error);
-    }
-  };
   return (
     <div className='container'>
       <h2>Регистрация</h2>
@@ -83,7 +24,18 @@ const Register = () => {
         onChange={(e) => setPassword(e.target.value)}
         placeholder='Пароль'
       />
-      <button onClick={register}>Зарегистрироваться</button>
+      <button
+        onClick={() => {
+          register({
+            username,
+            password,
+            setRegistrationMessage,
+            navigate,
+          });
+        }}
+      >
+        Зарегистрироваться
+      </button>
       {registrationMessage && <div>{registrationMessage}</div>}{" "}
       {/* Сообщение о регистрации */}
     </div>
